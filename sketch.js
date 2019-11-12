a281734475ea5e4b4c6b5355780ad8a3c69f5034
jhlee981202 <abcxyz100@naver.com>
2019-11-12 11:18:56 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,9 +6,7 @@ function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor();
-  fill(255);
-textAlign(CENTER);
-text('Click to create a new sprite', width/2, height/2);
+
 //draw all the sprites added to the sketch so far
 //the positions will be updated automatically at every cycle
 drawSprites();
@@ -17,6 +15,9 @@ drawSprites();
 function draw() {
   background(30);
   ellipse(mouseX, mouseY, 100, 100);
+  fill(255);
+textAlign(CENTER);
+text('Click to create a new sprite', width/2, height/2);
 }
 
 function mousePressed() {
