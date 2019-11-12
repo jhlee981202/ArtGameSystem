db6e78d44a2ecf6d5a3d41adcb2c2c514219e37a
jhlee981202 <abcxyz100@naver.com>
2019-11-12 11:12:44 +0900

    Update index.html

--- sketch.js
+++ sketch.js
@@ -12,3 +12,16 @@ function draw() {
   background(30);
   ellipse(mouseX, mouseY, 100, 100);
 }
+
+function mousePressed() {
+
+  //create a sprite at the mouse position and store it in a temporary variable
+  var s = createSprite(mouseX, mouseY, 30, 30);
+  //if no image or animation is associated it will be a rectancle of the specified size
+  //and a random color
+
+  //now you can use the variable to set properties
+  //e.g. a random velocity on the x and y coordinates
+  s.velocity.x = random(-5, 5);
+  s.velocity.y = random(-5, 5);
+}
