4df991ea5ad5ecbdecd3f965e56980d3493c68d5
jhlee981202 <abcxyz100@naver.com>
2019-12-16 19:38:37 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,7 +17,10 @@ function draw()
   if (frameCount % 60 == 0 && time >= 0 && mouseIsPressed) {
     time ++;
   }
- if(time>10 && time<20){
- text('멸치',100,100)
+ if(time>5 && time<10){
+ text('해양쓰레기',100,100)
  }
+  if(time>10 && time<20){
+  text('멸치',100,100) || text('날치',100,100)
+  }
 }
